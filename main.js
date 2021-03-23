3721136928b9b63860eea2d5320a80f33bd8fa7f
Manoj Choudhury <aj.manoj99@gmail.com>
2021-03-23 21:06:59 +0000

    Setting up flowabaord public  page

---
+++ main.js
@@ -0,0 +1,18 @@
+
+import * as architecture from 'https://raw.githubusercontent.com/flow-aboard/flowaboard/master/front-end/data/architecture/architecture.js';
+import * as ui from 'https://raw.githubusercontent.com/flow-aboard/flowaboard/master/front-end/ui/export.js'
+import { Flow } from 'https://raw.githubusercontent.com/flow-aboard/flowaboard/master/front-end/ui/element-group/flow.js'
+import { UI } from 'https://raw.githubusercontent.com/flow-aboard/flowaboard/master/front-end/data/architecture/ui.js'
+import { Database } from 'https://raw.githubusercontent.com/flow-aboard/flowaboard/master/front-end/data/architecture/database.js'
+
+var design = new architecture.Design()
+design.addProcess(new architecture.Process('js','Process','process'))
+
+const flow = Flow.getNewInstance();
+document.body.appendChild(flow)
+flow.value=design;
+design.subscribe('change',(e)=>{
+    flow.handleValueChange()
+})
+flow.activeWidth="60%"
+flow.activeHeight="60%"
